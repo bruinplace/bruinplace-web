e91502cd76bdc098e73cfcea453111478d909a62
Melinda <melindaanniexu@gmail.com>
2026-03-02 21:16:27 -0800

    next error

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,7 +1,11 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function SearchLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <section className="search-layout">
+      {children}
+    </section>
   )
 }
